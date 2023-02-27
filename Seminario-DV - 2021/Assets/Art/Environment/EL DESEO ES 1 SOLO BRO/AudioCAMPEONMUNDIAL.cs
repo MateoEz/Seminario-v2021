8fd7929d78912ed9c9688b9da8d4dba7c76a6ba6
AgusFont <57245720+AgusFont@users.noreply.github.com>
2023-02-26 22:26:06 -0300

    mateo puto

--- Seminario-DV - 2021/Assets/Art/Environment/EL DESEO ES 1 SOLO BRO/AudioCAMPEONMUNDIAL.cs
+++ Seminario-DV - 2021/Assets/Art/Environment/EL DESEO ES 1 SOLO BRO/AudioCAMPEONMUNDIAL.cs
@@ -7,13 +7,20 @@ public class AudioCAMPEONMUNDIAL : MonoBehaviour
 
     [SerializeField] AudioSource audioSong;
     [SerializeField] AudioSource audioWorldChampion;
+    [SerializeField] GameObject particle1;
 
+    private void Start()
+    {
+        Debug.Log("arranque");
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.TryGetComponent(out PlayerView playerView) || other.TryGetComponent(out DashPlayerFeedback dashPlayerFeedback))
         {
+            Debug.Log("entre wacho");
             audioSong.gameObject.SetActive(false);
             audioWorldChampion.gameObject.SetActive(true);
+            particle1.SetActive(true);
         }
     }
     private void OnTriggerExit(Collider other)
@@ -21,8 +28,8 @@ public class AudioCAMPEONMUNDIAL : MonoBehaviour
         if (other.TryGetComponent(out PlayerView playerView) || other.TryGetComponent(out DashPlayerFeedback dashPlayerFeedback))
         {
             audioSong.gameObject.SetActive(true);
-        audioWorldChampion.gameObject.SetActive(false);
-
+            audioWorldChampion.gameObject.SetActive(false);
+            particle1.SetActive(false);
         }
     }
 }
