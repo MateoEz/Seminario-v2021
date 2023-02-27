fc2725fc59891d2b48cd4a8bf8467abb5584d1ee
Mateo García <mateo.ezequiel.garcia@gmail.com>
2023-02-27 17:01:18 -0300

    Update CollectibleBehaviour.cs

--- Seminario-DV - 2021/Assets/CollectibleBehaviour.cs
+++ Seminario-DV - 2021/Assets/CollectibleBehaviour.cs
@@ -11,17 +11,24 @@ public class CollectibleBehaviour : MonoBehaviour
     [SerializeField] private AudioSource myAudioSource;
     [SerializeField] private AudioClip myAudioClip;
 
+
+    private bool _canBeGrabbed = false;
+    private void Update()
+    {
+        if (!_canBeGrabbed) return;
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            OnTouched();
+        }
+    }
+
     private void OnTriggerStay(Collider other)
     {
         if (other.TryGetComponent(out PlayerView playerView) ||
             other.TryGetComponent(out DashPlayerFeedback dashPlayerFeedback))
         {
+            _canBeGrabbed = true;
             
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                OnTouched();
-            }
-
             if (particle)
             {
                 if (particle.activeInHierarchy) return;
@@ -36,11 +43,13 @@ public class CollectibleBehaviour : MonoBehaviour
             other.TryGetComponent(out DashPlayerFeedback dashPlayerFeedback))
         {
             particle.SetActive(false);
+            _canBeGrabbed = false;
         }
     }
 
     private void OnTouched()
     {
+        _canBeGrabbed = false;
         GetComponent<BoxCollider>().enabled = false;
         gameObjectToDeactivate.SetActive(false);
         Destroy(particle);
