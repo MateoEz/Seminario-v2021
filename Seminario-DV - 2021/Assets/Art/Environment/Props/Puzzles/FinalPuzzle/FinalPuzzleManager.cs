7e4b682db1940399b57428d84bf97bb1b0c23c78
AgusFont <57245720+AgusFont@users.noreply.github.com>
2023-03-03 05:18:05 -0300

    PuzzleAndBoss

---
+++ Seminario-DV - 2021/Assets/Art/Environment/Props/Puzzles/FinalPuzzle/FinalPuzzleManager.cs
@@ -0,0 +1,122 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class FinalPuzzleManager : MonoBehaviour
+{
+
+    [SerializeField] List<int> answer = new List<int>();
+    [SerializeField] List<int> answersToCheck = new List<int>();
+    [SerializeField] List<FinalPuzzleColum> colums = new List<FinalPuzzleColum>();
+
+    [SerializeField] GameObject puzzleWall;
+    [SerializeField] Material puzzleCompletedMat;
+
+    [SerializeField] MoveTo door;
+    [SerializeField] CableFeedback cables;
+
+    public bool on;
+    public int correctAnswer;
+    bool completed;
+    private void Update()
+    {
+        if (!on) return;
+        ShowSequence();
+        if (answersToCheck.Count ==answer.Count)
+        {
+            CheckSequence();
+        }
+    }
+    public void AddToSequence(FinalPuzzleColum colum,int id)
+    {
+        colums.Add(colum);
+        answersToCheck.Add(id);
+    }
+
+    public void CheckSequence()
+    {
+        if (completed) return;
+        for (int i = 0; i < answer.Count; i++)
+        {
+            if (answer[i].Equals(answersToCheck[i]))
+            {
+                correctAnswer++;
+                Debug.Log("ta bien este");
+            }
+            else Debug.Log("ta mal");
+        }
+        if (correctAnswer >=answer.Count)
+        {
+            CorrectAnswer();
+            Debug.Log("completado");
+        }
+        else
+        {
+            WrongAnswer();
+            Debug.Log("fracasado");
+        }
+
+    }
+
+    public void CorrectAnswer()
+    {
+        AudioMaster.Instance.PlayClip("PuzzleCompleted", .8f);
+        completed = true;
+        //door.moveColumn = true;
+        puzzleWall.GetComponent<MeshRenderer>().material = puzzleCompletedMat;
+        cables.NeedFeedBackFromCables();
+    }
+
+    public void ClearSequence()
+    {
+        colums.Clear();
+        answersToCheck.Clear();
+    }
+
+    public void WrongAnswer()
+    {
+        foreach (var item in colums)
+        {
+            item.TurnRed();
+        }
+        ClearSequence();
+    }
+
+    public void ResetMats()
+    {
+        foreach (var item in colums)
+        {
+            item.ResetColor();
+        }
+    }
+
+    [SerializeField] List<GameObject> simbols = new List<GameObject>();
+    bool activatedSequece;
+    public void ShowSequence()
+    {
+        if (activatedSequece) return;
+        float myTick = 0;
+        for (int i = 0; i < simbols.Count; i++)
+        {
+           
+            StartCoroutine(BrightObject(simbols[i] ,myTick));
+            myTick += 2;
+        }
+        activatedSequece = true;
+    }
+    IEnumerator BrightObject(GameObject simbol,float tick)
+    {
+        yield return new WaitForSeconds(tick);
+        simbol.GetComponent<Animator>().SetTrigger("BrightTime");
+        if (simbol == simbols[simbols.Count-1])
+        {
+            yield return new WaitForSeconds(3.5f);
+            activatedSequece = false;
+            foreach (var item in simbols)
+            {
+                item.GetComponent<Animator>().SetTrigger("DarkTime");
+            }
+        }
+    }
+}
